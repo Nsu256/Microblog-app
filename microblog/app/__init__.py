64d14486c563e34c06aa36c53a283e8b0e622a21
NSUBUGA EDIGER <nsubugaediger@gmail.com>
2026-02-26 16:54:25 +0000

    debugging of the form.py directory

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-form config import Config
+from config import Config
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config.from_object(Config)
 
 from app import routes
--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Datarequired
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
