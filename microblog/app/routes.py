a0fba7f7a0d22da263971eca0542c7cd263cc56d
NSUBUGA EDIGER <nsubugaediger@gmail.com>
2026-02-27 12:40:23 +0000

    Generating links instead of typing them manually

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -12,10 +12,7 @@ from app.forms import LoginForm
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+        return redirect(url_for('index'))
 
 @app.route('/')
 @app.route('/index')
