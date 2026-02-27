c1da689367b5c1c43268c52bf6bd5638f2d9fa24
NSUBUGA EDIGER <nsubugaediger@gmail.com>
2026-02-27 12:48:18 +0000

    Debugging of my function that will generate the links

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -13,6 +13,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/')
 @app.route('/index')
